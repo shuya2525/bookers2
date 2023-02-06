bfc914e67a84c8af8a6c411c4a4d13c6306d51a9
Shuya <105729756+shuya2525@users.noreply.github.com>
2023-02-06 16:20:27 +0000

    comment,favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
